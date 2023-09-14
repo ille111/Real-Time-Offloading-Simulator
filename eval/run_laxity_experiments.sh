a97cf43399e2405c3ce0170233299ad3cf671dfb
Ilja Behnke <user@dhcp-248-214.cit.tu-berlin.de>
2023-09-14 12:52:14 +0200

    analyzed

--- eval/run_laxity_experiments.sh
+++ eval/run_laxity_experiments.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-python3 middlewedge_experiments/laxity_test.py --laxity=180 --uf=2.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_10_2.5
-python3 middlewedge_experiments/laxity_test.py --laxity=60 --uf=2.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_20_2.5
-python3 middlewedge_experiments/laxity_test.py --laxity=90 --uf=2.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_30_2.5
-python3 middlewedge_experiments/laxity_test.py --laxity=120 --uf=2.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_40_2.5
-python3 middlewedge_experiments/laxity_test.py --laxity=150 --uf=2.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_50_2.5
-python3 middlewedge_experiments/laxity_test.py --laxity=180 --uf=1.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_10_1.5
-python3 middlewedge_experiments/laxity_test.py --laxity=60 --uf=1.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_20_1.5
-python3 middlewedge_experiments/laxity_test.py --laxity=90 --uf=1.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_30_1.5
-python3 middlewedge_experiments/laxity_test.py --laxity=120 --uf=1.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_40_1.5
-python3 middlewedge_experiments/laxity_test.py --laxity=150 --uf=1.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_50_1.5
-python3 middlewedge_experiments/laxity_test.py --laxity=180 --uf=0.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_10_0.5
-python3 middlewedge_experiments/laxity_test.py --laxity=60 --uf=0.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_20_0.5
-python3 middlewedge_experiments/laxity_test.py --laxity=90 --uf=0.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_30_0.5
-python3 middlewedge_experiments/laxity_test.py --laxity=120 --uf=0.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_40_0.5
-python3 middlewedge_experiments/laxity_test.py --laxity=150 --uf=0.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_50_0.5
+python3 middlewedge_experiments/laxity_test.py --laxity=180 --uf=2.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_180_2.5
+python3 middlewedge_experiments/laxity_test.py --laxity=60 --uf=2.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_60_2.5
+python3 middlewedge_experiments/laxity_test.py --laxity=90 --uf=2.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_90_2.5
+python3 middlewedge_experiments/laxity_test.py --laxity=160 --uf=2.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_160_2.5
+python3 middlewedge_experiments/laxity_test.py --laxity=150 --uf=2.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_150_2.5
+python3 middlewedge_experiments/laxity_test.py --laxity=180 --uf=1.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_180_1.5
+python3 middlewedge_experiments/laxity_test.py --laxity=60 --uf=1.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_60_1.5
+python3 middlewedge_experiments/laxity_test.py --laxity=90 --uf=1.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_90_1.5
+python3 middlewedge_experiments/laxity_test.py --laxity=160 --uf=1.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_160_1.5
+python3 middlewedge_experiments/laxity_test.py --laxity=150 --uf=1.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_150_1.5
+python3 middlewedge_experiments/laxity_test.py --laxity=180 --uf=0.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_180_0.5
+python3 middlewedge_experiments/laxity_test.py --laxity=60 --uf=0.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_60_0.5
+python3 middlewedge_experiments/laxity_test.py --laxity=90 --uf=0.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_90_0.5
+python3 middlewedge_experiments/laxity_test.py --laxity=160 --uf=0.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_160_0.5
+python3 middlewedge_experiments/laxity_test.py --laxity=150 --uf=0.5 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_150_0.5
 #python3 middlewedge_experiments/laxity_test.py --laxity=180 --uf=4.0 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_180_4
 #python3 middlewedge_experiments/laxity_test.py --laxity=60 --uf=4.0 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_60_4
 #python3 middlewedge_experiments/laxity_test.py --laxity=90 --uf=4.0 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_90_4
-#python3 middlewedge_experiments/laxity_test.py --laxity=120 --uf=4.0 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_120_4
+#python3 middlewedge_experiments/laxity_test.py --laxity=160 --uf=4.0 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_160_4
 #python3 middlewedge_experiments/laxity_test.py --laxity=150 --uf=4.0 --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_150_4
 #python3 middlewedge_experiments/laxity_test.py --laxity=180 --uf=1.0 -b --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_180_b
 #python3 middlewedge_experiments/laxity_test.py --laxity=60 --uf=1.0 -b --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_60_b
 #python3 middlewedge_experiments/laxity_test.py --laxity=90 --uf=1.0 -b --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_90_b
-#python3 middlewedge_experiments/laxity_test.py --laxity=120 --uf=1.0 -b --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_120_b
+#python3 middlewedge_experiments/laxity_test.py --laxity=160 --uf=1.0 -b --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_160_b
 #python3 middlewedge_experiments/laxity_test.py --laxity=150 --uf=1.0 -b --log_dir=/home/mininet/Real-Time-Offloading-Simulator/eval/middlewedge_experiments/laxity_test_150_b
 
